53b57077aed0880ff7d421919c1b8d698614a244
root <root@Podarcis>
2020-06-17 12:27:36 +0200

    mutRate at a certain point

--- mutationRate.R
+++ mutationRate.R
@@ -26,7 +26,7 @@ sim1 <- oncoSimulIndiv(fe,
                       errorHitMaxTries = FALSE, 
                       errorHitWallTime = FALSE)
 
-plot(sim1, show = "genotypes", col = c("green", "red", "yellow"))                    
+plot(sim1, show = "genotypes", col = c("green", "red", "yellow"))
 ##############################################################
 
 set.seed(2)
@@ -51,7 +51,7 @@ sim3 <- oncoSimulIndiv(fe,
                       onlyCancer = FALSE, 
                       finalTime = 1000,
                       mu = 1e-6,
-                      muFactor = c(10000,400),
+                      muFactor = c(10000,200),
                       initSize = 5000, 
                       keepPhylog = FALSE,
                       seed = NULL, 
@@ -60,3 +60,57 @@ sim3 <- oncoSimulIndiv(fe,
 
 plot(sim3, show = "genotypes", col = c("purple", "red", "yellow"))
 #######################################################################
+
+#high to low mutation rate
+set.seed(2)
+sim4 <- oncoSimulIndiv(fe,
+                       model = "McFL", 
+                       onlyCancer = FALSE, 
+                       finalTime = 1000,
+                       mu = 1e-2,
+                       initSize = 5000, 
+                       keepPhylog = FALSE,
+                       seed = NULL, 
+                       errorHitMaxTries = FALSE, 
+                       errorHitWallTime = FALSE)
+
+plot(sim4, show = "genotypes", col = c("green", "red", "yellow"))
+##############################################################
+
+set.seed(2)
+sim5 <- oncoSimulIndiv(fe,
+                       model = "McFL", 
+                       onlyCancer = FALSE, 
+                       finalTime = 1000,
+                       mu = 1e-2,
+                       muFactor = c((1/1000),0),
+                       initSize = 5000, 
+                       keepPhylog = FALSE,
+                       seed = NULL, 
+                       errorHitMaxTries = FALSE, 
+                       errorHitWallTime = FALSE)
+
+plot(sim5, show = "genotypes", col = c("green", "red", "yellow"))
+###################################################################
+
+set.seed(2)
+sim6 <- oncoSimulIndiv(fe,
+                       model = "McFL", 
+                       onlyCancer = FALSE, 
+                       finalTime = 1000,
+                       mu = 1e-2,
+                       muFactor = c((1/10000),200),
+                       initSize = 5000, 
+                       keepPhylog = FALSE,
+                       seed = NULL, 
+                       errorHitMaxTries = FALSE, 
+                       errorHitWallTime = FALSE)
+
+plot(sim6, show = "genotypes", col = c("purple", "red", "yellow"))
+####################################################################
+sim1$pops.by.time[nrow(sim1$pops.by.time), ]
+sim2$pops.by.time[nrow(sim2$pops.by.time), ]
+sim3$pops.by.time[nrow(sim3$pops.by.time), ]
+sim4$pops.by.time[nrow(sim4$pops.by.time), ]
+sim5$pops.by.time[nrow(sim5$pops.by.time), ]
+sim6$pops.by.time[nrow(sim6$pops.by.time), ]
